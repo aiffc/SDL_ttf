3860955c3766c194fdf04043320448d06e972926
aiffc <yjc18915568435@gmail.com>
2025-08-12 16:33:53 +0800

    fix compile error

--- build.zig
+++ build.zig
@@ -54,10 +54,11 @@ pub fn build(b: *std.Build) void {
     });
 
     sdl_ttf_lib.addIncludePath(b.path("include/"));
-    sdl_ttf_lib.addSystemIncludePath(b.path("SDL3"));
+    sdl_ttf_lib.addSystemIncludePath(sdl_dep.path("include/"));
+    sdl_ttf_lib.addSystemIncludePath(freetype_dep.path("include/"));
 
     sdl_ttf_lib.linkLibrary(sdl_dep.artifact("SDL3"));
-    sdl_ttf_lib.linkLibrary(sdl_dep.artifact("freetype"));
+    sdl_ttf_lib.linkLibrary(freetype_dep.artifact("freetype"));
     sdl_ttf_lib.installHeadersDirectory(b.path("include/SDL3_ttf/"), "SDL3_ttf/", .{
         .exclude_extensions = &.{},
     });
